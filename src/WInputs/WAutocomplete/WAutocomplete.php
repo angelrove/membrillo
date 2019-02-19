497afde1b9ab6421f98198684fe01ca79740ecff
Jose Angel <jangel.romero@gmail.com>
2019-02-19 11:13:40 +0100

    WAutocomplete

--- src/WInputs/WAutocomplete/WAutocomplete.php
+++ src/WInputs/WAutocomplete/WAutocomplete.php
@@ -12,12 +12,14 @@ class WAutocomplete
 {
   private $input_name;
   private $input_id;
+  private $input_label;
 
   //---------------------------------------------------------------------
-  public function __construct($input_name, $input_id, $url_ajax)
+  public function __construct($input_name, $input_id, $label, $url_ajax)
   {
       $this->input_name = $input_name;
       $this->input_id   = $input_id;
+      $this->input_label= $label;
 
       CssJsLoad::set_script("
           var AUTOCOMPLETE_INPUT_NAME = '$input_name';
@@ -28,12 +30,12 @@ class WAutocomplete
       CssJsLoad::set(__DIR__ . '/libs.js');
   }
   //---------------------------------------------------------------------
-  public function get($form, $input_label) {
+  public function get($form) {
 
     return '
       <!-- WAutocomplete -->
-      <div id="WAutocomplete">
-        '.$form->getInput($this->input_name, $input_label, false).'
+      <div class="WAutocomplete" id="WAutocomplete_'.$this->input_name.'">
+        '.$form->getInput($this->input_name, $this->input_label, false).'
         '.$form->getInput($this->input_id, false, false, 'hidden').'
       </div>
       <!-- /WAutocomplete -->
--- src/WInputs/WAutocomplete/libs.js
+++ src/WInputs/WAutocomplete/libs.js
@@ -3,34 +3,34 @@ $(function() {
 
     $('[name="'+AUTOCOMPLETE_INPUT_NAME+'"]').autocomplete({
       source: function( request, response ) {
-        $.ajax({
-          url: AUTOCOMPLETE_URL_AJAX,
-          dataType: "jsonp",
-          data: {
-            q: request.term
-          },
-          success: function( data ) {
-            // console.log(data);
-            response( data );
-          }
-        });
+         $.ajax({
+           url: AUTOCOMPLETE_URL_AJAX,
+           dataType: "jsonp",
+           data: {
+              q: request.term
+           },
+           success: function( data ) {
+              // console.log(data);
+              response( data );
+           }
+         });
       },
 
       minLength: 3,
 
       select: function( event, ui ) {
-        console.log('id: '+ ui.item.value);
-        console.log('label: '+ ui.item.label);
+         console.log('id: '+ ui.item.value + '\n'+'label: '+ ui.item.label);
 
-        $('[name="'+AUTOCOMPLETE_INPUT_NAME+'"]').val(ui.item.label);
-        $('[name="'+AUTOCOMPLETE_INPUT_ID+'"]').val(ui.item.value);
+         $('[name="'+AUTOCOMPLETE_INPUT_NAME+'"]').val(ui.item.label);
+         $('[name="'+AUTOCOMPLETE_INPUT_ID+'"]').val(ui.item.value);
          return false;
       },
       open: function() {
-        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+         $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
       },
       close: function() {
-        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+         $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
       }
     });
+
 });
