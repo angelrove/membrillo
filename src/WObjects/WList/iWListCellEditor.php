4ec0c4e645e10ca66d15b81fa8d13592e92d17a5
Jose Angel Romero <jangel.romero@gmail.com>
2019-09-24 18:16:44 +0200

    improvements

--- src/WObjects/WList/iWListCellEditor.php
+++ src/WObjects/WList/iWListCellEditor.php
@@ -3,7 +3,7 @@ namespace angelrove\membrillo\WObjects\WList;
 
 interface iWListCellEditor
 {
-    public function getValueAt($id, $columnName, $value, $values);
+    public function getValueAt($id, $columnName, $value, &$values);
     public function getBgColorAt($id, $columnName, $value, $values);
     public function getOnClick($id, $columnName, $value,  $values);
 }
