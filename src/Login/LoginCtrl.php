be4e4f4d61cc5d60d64725a62a2506bc4c41e7ae
Jose Angel Romero <jangel.romero@gmail.com>
2019-12-17 13:34:18 +0100

    improvements

--- src/Login/LoginCtrl.php
+++ src/Login/LoginCtrl.php
@@ -8,6 +8,7 @@ namespace angelrove\membrillo\Login;
 
 use angelrove\utils\Db_mysql;
 use App\Models\User;
+use angelrove\utils\UtilsBasic;
 
 class LoginCtrl
 {
@@ -47,34 +48,80 @@ class LoginCtrl
         self::$iLoginQuery = $iLoginQuery;
     }
     //------------------------------------------------
+    private static function loginUser($LOGIN_USER, $LOGIN_PASSWD, $params)
+    {
+        global $CONFIG_APP;
+
+        $userData = array();
+
+        if (isset(self::$iLoginQuery)) {
+            $userData = self::$iLoginQuery->get($LOGIN_USER, $LOGIN_PASSWD, $params);
+        } else {
+            if ($CONFIG_APP['login']['LOGIN_HASH']) {
+                $userData = User::loginHash($LOGIN_USER, $LOGIN_PASSWD);
+            } else {
+                $userData = User::login($LOGIN_USER, $LOGIN_PASSWD);
+            }
+        }
+
+        // Login ok
+        if (isset($userData['id'])) {
+            new Login($userData['id'], $userData['email'], $userData);
+            header("Location: /?APP_EVENT=timezone");
+            exit();
+        }
+    }
+    //------------------------------------------------
+    private static function restorePass($LOGIN_USER)
+    {
+        global $CONFIG_APP;
+
+        // New password
+        $pass = UtilsBasic::randomPassword();
+        $hash = $pass;
+        if ($CONFIG_APP['login']['LOGIN_HASH']) {
+            $hash = password_hash($pass, PASSWORD_BCRYPT);
+        }
+
+        // Database update
+        $user = \DB::table('users')->where('email', '=', $LOGIN_USER)->first();
+        if (!$user) {
+            header("Location: /?message='xxx'");
+            exit();
+        }
+
+        \DB::table('users')
+            ->where('id', $user->id)
+            ->update([
+                'password' => $hash,
+            ]);
+
+        // Send mail with new password
+        $from = 'noreply@'.$CONFIG_APP['data']['domain'];
+        $mailto = $LOGIN_USER;
+        $bcc = '';
+        $subject = 'Restore password';
+        $body = '
+            Hello.
+            Here you have your new pass: '.$pass.'
+
+            <hr>
+            Please dont reply to this email.
+        ';
+
+        UtilsBasic::sendEMail($from, $mailto, $bcc, $subject, $body);
+       header("Location: /"); exit();
+    }
+    //------------------------------------------------
     public static function initPage()
     {
         global $CONFIG_APP;
 
-        // Authenticate --------------
-        $userData = array();
-
-        // Login -----------
-        if ((isset($_REQUEST['LOGIN_USER']) && $_REQUEST['LOGIN_USER']) ||
-            isset($_REQUEST['auth_token']) ) {
-            // User data
-            $userData = array();
-            if (isset(self::$iLoginQuery)) {
-                $userData = self::$iLoginQuery->get($_REQUEST['LOGIN_USER'], $_REQUEST['LOGIN_PASSWD'], $_REQUEST);
-            } else {
-                if ($CONFIG_APP['login']['LOGIN_HASH']) {
-                    $userData = User::loginHash($_REQUEST['LOGIN_USER'], $_REQUEST['LOGIN_PASSWD']);
-                } else {
-                    $userData = User::login($_REQUEST['LOGIN_USER'], $_REQUEST['LOGIN_PASSWD']);
-                }
-            }
-
-            // Login ok
-            if (isset($userData['id'])) {
-                new Login($userData['id'], $userData['email'], $userData);
-                header("Location: /?APP_EVENT=timezone");
-                exit();
-            }
+        // Authenticate ------------
+        if ((isset($_REQUEST['restorepass']) && $_REQUEST['restorepass']) && (isset($_REQUEST['LOGIN_USER']) && $_REQUEST['LOGIN_USER'])) {
+            self::restorePass($_REQUEST['LOGIN_USER']);
+        } elseif ((isset($_REQUEST['LOGIN_USER']) && $_REQUEST['LOGIN_USER']) || isset($_REQUEST['auth_token'])) {
+            self::loginUser($_REQUEST['LOGIN_USER'], $_REQUEST['LOGIN_PASSWD'], $_REQUEST);
         }
 
         // Authenticate view -------
