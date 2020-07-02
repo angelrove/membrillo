cd187ce9cc16f4aa127e2cc5dd33d9abab698857
Jose Angel Romero <jangel.romero@gmail.com>
2020-07-02 12:37:09 +0200

    improvements

--- src/print_r2.php
+++ src/print_r2.php
@@ -5,11 +5,10 @@ function print_r2($object, bool $setComment = false)
     $varDump = htmlspecialchars(print_r($object, true));
 
     if ($setComment === true) {
-        $ret = "\n<!--\n" . $varDump . "\n-->\n";
+        echo "\n<!--\n" . $varDump . "\n-->\n";
     } else {
-        $ret = '<span style="font-size:10px"><pre style="text-align:left">' . $varDump . '</pre></span>';
+        echo '<span style="font-size:10px"><pre style="text-align:left">' . $varDump . '</pre></span>';
     }
-    echo $ret;
 }
 //------------------------------------------------------------------
 function dd($object, bool $setComment = false)
