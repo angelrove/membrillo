82df204d4526f3f65fedc258989b34002c6d56ea
Jose Angel Romero <jangel.romero@gmail.com>
2020-07-15 17:14:08 +0200

    update

--- src/Magic/NewSecc/files/Models/Sample.php
+++ src/Magic/NewSecc/files/Models/Sample.php
@@ -1,27 +1,56 @@
 <?php
 namespace App\Models;
 
-use angelrove\membrillo\Database\Model;
-use angelrove\membrillo\Database\GenQuery;
+use angelrove\membrillo\Eloquent\ModelPlus;
+use Illuminate\Database\Eloquent\SoftDeletes;
+// use App\Models\Eloquent\Scopes\CompanyScope;
 
-class [name_model] extends Model
+class [name_model] extends ModelPlus
 {
-    protected const CONF = array(
-        'table' => '[name_table]',
-        'soft_delete' => true,
-    );
+    use SoftDeletes;
 
-    // public static function read(array $conditions=array(), array $filter_data=array())
-    // {
-    //     $conditions[] = "id <> 1";
-    //
-    //     // Search ---
-    //     $conditions['f_text'] = "name LIKE '%[VALUE]%'";
-    //     $conditions['f_deleted'] = [
-    //         'default' => "deleted_at IS NULL",
-    //         1 => "deleted_at IS NOT NULL",
-    //     ];
+    protected $table = "[name_table]";
 
-    //     return parent::read($conditions, $filter_data);
-    // }
+    public static $profiles = [
+       'admin' => 'Admin',
+       'basic' => 'Basic',
+    ];
+
+    // protected $fillable = [
+    // ];
+
+    protected $guarded = [
+    ];
+
+    //-------------------------------------------------------
+    // Relationships
+    //-------------------------------------------------------
+    public function department()
+    {
+        // return $this->belongsTo('App\Models\Eloquent\UserDepartment');
+    }
+
+    //-------------------------------------------------------
+    // Global Scopes
+    //-------------------------------------------------------
+    protected static function boot()
+    {
+        parent::boot();
+
+        // static::addGlobalScope(new CompanyScope);
+    }
+
+    //-------------------------------------------------------
+    // Scopes
+    //-------------------------------------------------------
+    public function scopeSearch($query, Request $request)
+    {
+        $inputs = $request->only(['name']);
+
+        foreach ($inputs as $key => $value) {
+            $query->where($key, 'LIKE', '%' . $value . '%');
+        }
+
+        return $query;
+    }
 }
